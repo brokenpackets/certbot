fe33bc637bf9c9865177595c846b257d073ed5a7
Tyler Conrad <36974147+brokenpackets@users.noreply.github.com>
2019-02-05 11:54:57 -0800

    Change method to base64.
    Use base64 rather than line-by-line api copying.

--- certbot.py
+++ certbot.py
@@ -3,6 +3,7 @@ from getpass import getpass
 from jsonrpclib import Server
 import json
 import ssl
+import base64
 
 #CREDS
 user = raw_input("Enter username: ")
@@ -29,7 +30,8 @@ MIIGJTCCBA2gAwIBAgIBAjANBgkqhkiG9w0BAQUFADCBsjELMAkGA1UEBhMCRlIx
 MIIGJTCCBA2gAwIBAgIBAjANBgkqhkiG9w0BAQUFADCBsjELMAkGA1UEBhMCRlIx
 MIIGJTCCBA2gAwIBAgIBAjANBgkqhkiG9w0BAQUFADCBsjELMAkGA1UEBhMCRlIx
 -----END CERTIFICATE-----"""
-certparsed = cert.split('\n')
+cert_encoded = cert.encode('base64','strict')
+cert_stripped = cert_encoded.replace('\n','')
 
 cert_key = """\
 -----BEGIN RSA PRIVATE KEY-----
@@ -46,7 +48,8 @@ MIIGJTCCBA2gAwIBAgIBAjANBgkqhkiG9w0BAQUFADCBsjELMAkGA1UEBhMCRlIx
 MIIGJTCCBA2gAwIBAgIBAjANBgkqhkiG9w0BAQUFADCBsjELMAkGA1UEBhMCRlIx
 MIIGJTCCBA2gAwIBAgIBAjANBgkqhkiG9w0BAQUFADCBsjELMAkGA1UEBhMCRlIx
 -----END RSA PRIVATE KEY-----"""
-key_parsed = cert_key.split('\n')
+key_encoded = cert_key.encode('base64','strict')
+key_stripped = key_encoded.replace('\n','')
 
 def main():
   for SwitchNumber in dev_list:
@@ -57,18 +60,16 @@ def main():
 
     #CONNECT TO DEVICE
     try:
-      remove_certificate = ss.runCmds ( 1, [ 'enable', 'bash timeout 10 rm -rf /tmp/cert.cer'])
-      remove_key = ss.runCmds ( 1, [ 'enable', 'bash timeout 10 rm -rf /tmp/cert.key'])
-      for line in certparsed:
-        upload_cert = ss.runCmds ( 1, [ 'enable', 'bash timeout 2 echo "'+line+'" >> /tmp/cert.cer'])
-      for line in key_parsed:
-        upload_cert = ss.runCmds ( 1, [ 'enable', 'bash timeout 2 echo "'+line+'" >> /tmp/cert.key'])
+      remove_certificate = ss.runCmds ( 1, [ 'enable', 'bash timeout 10 rm -rf /tmp/cert*'])
+      upload_cert = ss.runCmds ( 1, [ 'enable', 'bash timeout 2 echo "'+cert_stripped+'" >> /tmp/certcer.tmp', 'bash timeout 2 base64 -d /tmp/certcer.tmp > /tmp/cert.cer'])
+      upload_key = ss.runCmds ( 1, [ 'enable', 'bash timeout 2 echo "'+key_stripped+'" >> /tmp/certkey.tmp', 'bash timeout 2 base64 -d /tmp/certkey.tmp > /tmp/cert.key'])
       response = ss.runCmds( 1, [ 'enable', 'copy file:/tmp/cert.cer certificate:cert',
                                   'copy file:/tmp/cert.key sslkey:certkey'])
       response2 = ss.runCmds( 1, [ 'enable', 'configure', 'management security',
       'ssl profile https-secure', 'certificate cert key certkey', 'cipher-list HIGH:!NULL:!MD5:!aNULL' ])
       response3 = ss.runCmds( 1, [ 'enable', 'configure', 'management api http-commands',
       'protocol https ssl profile https-secure' ])
+      remove_certificate = ss.runCmds ( 1, [ 'enable', 'bash timeout 10 rm -rf /tmp/cert*'])
       print 'Success on '+ip
     except:
       print 'Failure on '+ip
